22e56fb40dce8c63c05e4fd4047a2e138f270dd2
Sofie Nordahl Erner <sofie@erner.dk>
2025-02-03 15:51:55 +0000

    Initial unittest of get_items function using example given in coding exercise

---
+++ tests/Pricing_test.py
@@ -0,0 +1,43 @@
+import unittest
+import os
+import sys
+
+path = os.path.abspath(os.getcwd()) # path to directory of script
+sys.path.append("../")
+
+import src
+from src.get_items import get_items
+
+class get_items_test(unittest.TestCase):
+    """
+    get_items_test class to test the function
+    check against example given in coding exercise
+    """
+
+    # ----- Set up -----
+    def setUp(self):
+        """ Test result of function and list of keys to compare to """
+        test_list = [1,1,1,2,2,(3,0.2)] # list from coding exercise example
+
+        self.keys_exp = [1, 2, 3] # expected keys of dictionary
+        self.subTotal_exp = 3.3 # expected sub-total
+        self.dict_exp = {1: [3, 1], 2: [2, 2], 3: [1, 0]} # expected items dictionary
+
+        self.dict_result, self.subTotal = get_items(test_list) # results of function for test list
+
+    # ----- Test items dictionary -----
+    def test_dict_keys(self):
+        """ Test items dictionary keys """
+        self.assertListEqual(list(self.dict_result.keys()),self.keys_exp)
+
+    def test_dict_type(self):
+        """ Test output is a dictionary """
+        self.assertIs(type(self.dict_result),dict)
+
+    # ----- Test sub-total -----
+    def test_subTotal(self):
+        """ Test sub-total value """
+        self.assertEqual(round(self.subTotal,2), self.subTotal_exp)
+
+if __name__ == "__main__":
+    unittest.main()
