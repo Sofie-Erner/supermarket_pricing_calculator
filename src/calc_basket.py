115dfc88fa39e7c75898c513165781ab9aa95ec7
Sofie Nordahl Erner <sofie@erner.dk>
2025-02-03 15:52:38 +0000

    Implemented get_items into main function to get items dictionary and sub-total

--- src/calc_basket.py
+++ src/calc_basket.py
@@ -3,14 +3,26 @@ This file contains the function to calculate the price of a shopping basket.
 
 Input: list of shopping items
 Output: total price of basket
-
 """
-
 import src
-from price_dict import prices # get dictionary with prices
-from savings_dict import savings # get dictionary with savings
+from src.get_items import get_items # function to get dictionary of items and sub-total
 
 # ------ Function to calculate price for a shopping basket -----
 def calc_basket(shop_basket):
     print(shop_basket)
+    # --- Variables ---
+    savings = 0 # sum of savings
+    total = 0 # subTotal minus savings, final price of basket
+
+    # subtotal for sum of item prices without savings
+    # items: dictionary item number as key and list of occurrences and potential savings code as value
+
+    # --- Get dictionary of items & Sub-total ---
+    items, subTotal = get_items(shop_basket)
+        
+    print(items)
+    print(round(subTotal,2))
+
+    # --- Get Savings  ---
+
     return 0
